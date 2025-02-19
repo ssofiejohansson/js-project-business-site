86ff5551f2a19037f5e2932e5cfb4d95aefa563d
Sofie <sofieannamatilda@hotmail.se>
2025-02-19 19:28:32 +0100

    last js changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-function hamburgerMenu() {
-  const navLinks = document.getElementById('navLinks')
-  const menuIcon = document.getElementById('menuIcon')
+document.getElementById('menuIcon').addEventListener('click', function () {
+  const navLinks = document.getElementById('navLinks');
+  const menuIcon = document.getElementById('menuIcon');
 
-  // Toggle between two icons
-  navLinks.classList.toggle('active')
+  navLinks.classList.toggle('active');
 
-  if (navLinks.classList.contains('active')) {
-    menuIcon.classList.remove('fa-bars')
-    menuIcon.classList.add('fa-times')
-  } else {
-    menuIcon.classList.remove('fa-times')
-    menuIcon.classList.add('fa-bars')
-  }
-}
+  menuIcon.classList.toggle('fa-bars');
+  menuIcon.classList.toggle('fa-times');
+});
